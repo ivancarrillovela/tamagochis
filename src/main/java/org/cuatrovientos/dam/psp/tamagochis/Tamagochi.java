ef8f974168bed065bdabaddac2d832496fc7da2d
Ivan <ivancarrillovela@gmail.com>
2025-11-03 09:25:18 +0100

    Creado enum para el estado del tamagochi, constructor y algunos atributos que creo que seran necesarios

--- src/main/java/org/cuatrovientos/dam/psp/tamagochis/Tamagochi.java
+++ src/main/java/org/cuatrovientos/dam/psp/tamagochis/Tamagochi.java
@@ -1,5 +1,32 @@
 package org.cuatrovientos.dam.psp.tamagochis;
 
-public class Tamagochi {
+import java.util.Random;
+
+public class Tamagochi implements Runnable {
+	
+	public enum Estado{
+		COMIENTO,
+		JUGANDO,
+		DUCHANDOSE,
+		ESPERANDO,
+	}
+	
+	private Random rnd = new Random();
+	private String nombre;
+	private long horaNacimiento = System.currentTimeMillis();
+	private Estado estadoActual = Estado.ESPERANDO;
+	private boolean estaVivo = true;
+	
+	
+	public Tamagochi(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+
+	@Override
+	public void run() {
+
+		
+	}
 
 }
