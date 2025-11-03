2efc05d09c2353adeb799f788186ccbfca876c60
Ivan <ivancarrillovela@gmail.com>
2025-11-03 09:38:21 +0100

    Creado metodo darDeComer en Tamagochi para gestionar que coman tardando de comer un tiempo aleatorio

--- src/main/java/org/cuatrovientos/dam/psp/tamagochis/Tamagochi.java
+++ src/main/java/org/cuatrovientos/dam/psp/tamagochis/Tamagochi.java
@@ -3,21 +3,19 @@ package org.cuatrovientos.dam.psp.tamagochis;
 import java.util.Random;
 
 public class Tamagochi implements Runnable {
-	
-	public enum Estado{
-		COMIENTO,
-		JUGANDO,
-		DUCHANDOSE,
-		ESPERANDO,
+
+	public enum Estado {
+		COMIENDO, JUGANDO, DUCHANDOSE, ESPERANDO,
 	}
-	
+
 	private Random rnd = new Random();
 	private String nombre;
 	private long horaNacimiento = System.currentTimeMillis();
 	private Estado estadoActual = Estado.ESPERANDO;
 	private boolean estaVivo = true;
-	
-	
+
+	private final int MAX_TIEMPO_PARA_COMER = 120;
+
 	public Tamagochi(String nombre) {
 		super();
 		this.nombre = nombre;
@@ -26,7 +24,33 @@ public class Tamagochi implements Runnable {
 	@Override
 	public void run() {
 
+	}
+
+	public synchronized void darDeComer() {
 		
+		if (estadoActual != Estado.ESPERANDO) {
+			System.out.println(nombre + " ahora mismo esta " + estadoActual.toString() + " ¡Tienes que esperar a que termine!");
+		}
+
+		try {
+
+			int tiempoEnComer = rnd.nextInt(MAX_TIEMPO_PARA_COMER);
+			estadoActual = Estado.COMIENDO;
+
+			System.out.println("¡" + nombre + "ha empezado a comer!");
+			Thread.sleep(tiempoEnComer);
+			System.out.println("¡" + nombre + "ha terminado de comer!");
+
+		} catch (InterruptedException e) {
+
+			System.out.println(nombre + "ha sido interrumpido mientras comía...");
+			
+		} finally {
+
+			estadoActual = Estado.ESPERANDO;
+
+		}
+
 	}
 
 }
